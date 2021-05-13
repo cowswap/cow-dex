9eb097cc732442038c02ba40fcdad5bc4af1008b
huquduy <huquduy@gmail.com>
2021-05-14 01:57:55 +0700

    remove console

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -42,16 +42,16 @@ const useGetPriceData = () => {
 export const useGetPriceDataFromLP = () => {
   const wbnbAddress = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
   const bnbPrice = useGetPriceData()?.data?.[wbnbAddress].price
-  console.log(bnbPrice)
+  // console.log(bnbPrice)
   const [responseGoudaBnb] = useMultipleContractSingleData([GOUDA_BNB_POOL], PAIR_INTERFACE, 'getReserves')
-  console.log(responseGoudaBnb)
+  // console.log(responseGoudaBnb)
   if (responseGoudaBnb.loading === false && bnbPrice) {
     const resultGoudaBnb = responseGoudaBnb.result as Result;
     if (resultGoudaBnb) {
       const bananaReserve = new BigNumberJs(resultGoudaBnb[0]._hex)
       const bnbReserve = new BigNumberJs(resultGoudaBnb[1]._hex)
       const goudaUsd = bnbReserve.div(bananaReserve)
-      console.log(goudaUsd.toNumber())
+      // console.log(goudaUsd.toNumber())
       return new BigNumberJs(bnbPrice).times(goudaUsd).toNumber()
     }
   }
